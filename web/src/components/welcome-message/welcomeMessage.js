a396de3209ea51e0aaec80be6060e8a88a2ede9f
mr-jackpot <adam.heeps@live.co.uk>
2023-04-16 21:55:24 +0100

    Boilerplate site spun up.

---
+++ web/src/components/welcome-message/welcomeMessage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./welcomeMessage.css";
+
+class WelcomeMessage extends React.Component {
+  render() {
+    return (
+      <div className="welcomeMessage">
+        <h2>Welcome back!</h2>
+      </div>
+    );
+  }
+}
+
+export default WelcomeMessage;
---
+++ web/src/views/main/Main.js
@@ -0,0 +1,19 @@
+import './Main.css';
+import welcomeMessage from '../../components/welcome-message/welcomeMessage';
+import Summary from '../../components/summary/summary';
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import WelcomeMessage from '../../components/welcome-message/welcomeMessage';
+
+function Main() {
+  return (
+    <div className="Main">
+      <Header />
+      <WelcomeMessage />
+      <Summary />
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
