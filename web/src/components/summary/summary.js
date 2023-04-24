a6c3c7b779a8e33486bdd5f2ca47fb422218789f
mr-jackpot <adam.heeps@live.co.uk>
2023-04-24 20:00:36 +0100

    Updated summary files

--- web/src/components/summary/summary.js
+++ web/src/components/summary/summary.js
@@ -1,14 +1,78 @@
 import React from "react";
+import DataBreakdown from "../data-breakdown/dataBreakdown";
 import "./summary.css";
 
-class Summary extends React.Component {
+export default class Summary extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { summaryData: "Please select a week to view breakdown." };
+  }
+
+  handleClick(e) {
+    this.setState({summaryData: e.target.className})
+    console.log(e)
+  }
+
   render() {
     return (
-      <div className="summary">
-        <h2>This is the summary.</h2>
+      <div className="content-container">
+        <div className="summary-table">
+          <table>
+            <tr>
+              <th className="table-corner"></th>
+              <th>January</th>
+              <th>February</th>
+              <th>March</th>
+              <th>April</th>
+            </tr>
+            <tr>
+              <td className="week-heading">Week 1</td>
+              <td onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+            </tr>
+            <tr>
+              <td
+                onClick={this.handleClick.bind(this)}
+                className="week-heading"
+              >
+                Week 2
+              </td>
+              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <td onClick={this.handleClick.bind(this)} className="bad"></td>
+            </tr>
+            <tr>
+              <td
+                onClick={this.handleClick.bind(this)}
+                className="week-heading"
+              >
+                Week 3
+              </td>
+              <td onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+            </tr>
+            <tr>
+              <td
+                onClick={this.handleClick.bind(this)}
+                className="week-heading"
+              >
+                Week 4
+              </td>
+              <td onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td onClick={this.handleClick.bind(this)} className="good"></td>
+            </tr>
+          </table>
+        </div>
+        <DataBreakdown summaryData={this.state.summaryData} />
       </div>
     );
   }
 }
-
-export default Summary;
