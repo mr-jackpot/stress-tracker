4ff8a8ab0e32cb7a614889abff866d87e6a5c4c1
mr-jackpot <adam.heeps@live.co.uk>
2023-04-26 19:43:23 +0100

    Passing cell values to component

--- web/src/components/summary/summary.js
+++ web/src/components/summary/summary.js
@@ -3,15 +3,31 @@ import DataBreakdown from "../data-breakdown/dataBreakdown";
 import "./summary.css";
 
 export default class Summary extends React.Component {
-  
   constructor(props) {
     super(props);
-    this.state = { summaryData: "Please select a week to view breakdown." };
+    this.state = {
+      summaryData: {
+        loaded: false,
+        score: "default score",
+        week: "default week",
+        month: "default month",
+        holdingMessage: "Select a week to view breakdown.",
+      },
+    };
   }
 
   handleClick(e) {
-    this.setState({summaryData: e.target.className})
-    console.log(e)
+    const cell = e.target;
+    const header = cell.getAttribute("headers");
+    const rowHeader = cell.parentNode.querySelector('th[scope="row"]').textContent;
+    this.setState({
+      summaryData: {
+        loaded: true,
+        score: cell.className,
+        week: rowHeader,
+        month: header
+      },
+    });
   }
 
   render() {
@@ -21,53 +37,38 @@ export default class Summary extends React.Component {
           <table>
             <tr>
               <th className="table-corner"></th>
-              <th>January</th>
-              <th>February</th>
-              <th>March</th>
-              <th>April</th>
+              <th scope='column' id="January">January</th>
+              <th scope='column' id="February">February</th>
+              <th scope='column' id="March">March</th>
+              <th scope='column' id="April">April</th>
             </tr>
             <tr>
-              <td className="week-heading">Week 1</td>
-              <td onClick={this.handleClick.bind(this)} className="bad"></td>
-              <td onClick={this.handleClick.bind(this)} className="fair"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <th scope='row'>Week 1</th>
+              <td headers="January" onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td headers="February" onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td headers="March"  onClick={this.handleClick.bind(this)} className="good"></td>
+              <td headers="April"  onClick={this.handleClick.bind(this)} className="good"></td>
             </tr>
             <tr>
-              <td
-                onClick={this.handleClick.bind(this)}
-                className="week-heading"
-              >
-                Week 2
-              </td>
-              <td onClick={this.handleClick.bind(this)} className="fair"></td>
-              <td onClick={this.handleClick.bind(this)} className="fair"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
-              <td onClick={this.handleClick.bind(this)} className="bad"></td>
+              <th scope='row'>Week 2</th>
+              <td headers="January" onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td headers="February"onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td headers="March"  onClick={this.handleClick.bind(this)} className="good"></td>
+              <td headers="April"  onClick={this.handleClick.bind(this)} className="bad"></td>
             </tr>
             <tr>
-              <td
-                onClick={this.handleClick.bind(this)}
-                className="week-heading"
-              >
-                Week 3
-              </td>
-              <td onClick={this.handleClick.bind(this)} className="bad"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
-              <td onClick={this.handleClick.bind(this)} className="fair"></td>
+              <th scope='row'>Week 3</th>
+              <td headers="January" onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td headers="February"onClick={this.handleClick.bind(this)} className="good"></td>
+              <td headers="March"  onClick={this.handleClick.bind(this)} className="good"></td>
+              <td headers="April"  onClick={this.handleClick.bind(this)} className="fair"></td>
             </tr>
             <tr>
-              <td
-                onClick={this.handleClick.bind(this)}
-                className="week-heading"
-              >
-                Week 4
-              </td>
-              <td onClick={this.handleClick.bind(this)} className="bad"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
-              <td onClick={this.handleClick.bind(this)} className="fair"></td>
-              <td onClick={this.handleClick.bind(this)} className="good"></td>
+              <th scope='row'>Week 4</th>
+              <td headers="January" onClick={this.handleClick.bind(this)} className="bad"></td>
+              <td headers="February"onClick={this.handleClick.bind(this)} className="good"></td>
+              <td headers="March"  onClick={this.handleClick.bind(this)} className="fair"></td>
+              <td headers="April"  onClick={this.handleClick.bind(this)} className="good"></td>
             </tr>
           </table>
         </div>
