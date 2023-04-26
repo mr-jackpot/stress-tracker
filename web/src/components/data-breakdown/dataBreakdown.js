4ad67f7155b737836f13effc4d944b35aedd308e
mr-jackpot <adam.heeps@live.co.uk>
2023-04-26 19:43:40 +0100

    Retrieving values in props

--- web/src/components/data-breakdown/dataBreakdown.js
+++ web/src/components/data-breakdown/dataBreakdown.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./dataBreakdown.css";
 
 class DataBreakdown extends React.Component {
-
   render() {
-    return (
-      <div className="data-breakdown">
-        <p>{this.props.summaryData}</p>
+    return (  
+      <div className={`data-breakdown ${this.props.summaryData.score}`}>
+        {this.props.summaryData.loaded ?
+          <div className="title">Your score for {this.props.summaryData.month} {this.props.summaryData.week}:   
+            {"  "}{this.props.summaryData.score}</div> : 
+          <div className="holding-message">Please select a month to view data breakdown.</div>
+        }
+        
       </div>
     );
   }
