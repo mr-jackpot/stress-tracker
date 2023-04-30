b9ba554d22ac32769205235d118706adc3d5e433
mr-jackpot <adam.heeps@live.co.uk>
2023-04-30 17:31:36 +0100

    dark mode added

--- web/src/components/footer/footer.js
+++ web/src/components/footer/footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./footer.css";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <p>Stress Management Tracker application by Adam Heeps. Contact https://github.com/mr-jackpot</p>
-      </div>
-    );
-  }
-}
+const Footer = (props) => {
+
+  return (
+    <div className={`footer ${props.theme}`}>
+      <p>
+        Stress Management Tracker application by Adam Heeps. Contact
+        https://github.com/mr-jackpot
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
