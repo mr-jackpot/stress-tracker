464082154fcc269eeb68c3be641becf75a21ead2
mr-jackpot <adam.heeps@live.co.uk>
2023-06-03 21:21:07 +0100

    Major changes for databreakdown

--- web/src/components/data-breakdown/dataBreakdown.js
+++ web/src/components/data-breakdown/dataBreakdown.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./dataBreakdown.css";
+import TagIcon from "../tag-icon/tagIcon";
+
+const transformScore = (score) => {
+  return score.charAt(0).toUpperCase() + score.slice(1).toLowerCase();
+};
 
 const DataBreakdown = (props) => {
   return (
     <div className={`data-breakdown ${props.theme}`}>
       {props.summaryData.loaded ? (
-        <div className="title">
-          Your score for {props.summaryData.month}{" "}
-          {props.summaryData.week}:{"  "}
-          {props.summaryData.score}
+        <div className="message-container">
+          <div className="title">
+            Summary for {props.summaryData.month}{", "}
+            {props.summaryData.week}:
+          </div>
+          <div className="score-message">{transformScore(props.summaryData.score)}</div>
+          <div className="title">Themes for week</div>
+          <div className="tag-container">
+            <TagIcon feeling="shit" noclick/>
+          </div>
         </div>
       ) : (
         <div className="holding-message">
-          Please select a month to view data breakdown.
+          Please select a month to view data breakdown...
         </div>
       )}
     </div>
