2134bb1694dc289807cbec19649245733a74be4c
mr-jackpot <adam.heeps@live.co.uk>
2023-06-03 21:20:08 +0100

    Minor change to html table structure

--- web/src/components/summary/summary.js
+++ web/src/components/summary/summary.js
@@ -30,49 +30,51 @@ const Summary = (props) => {
     <div className="content-container">
       <div className="summary-table">
         <table>
-          <tr>
-            <th className="table-corner"></th>
-            <th scope="column" id="January">
-              January
-            </th>
-            <th scope="column" id="February">
-              February
-            </th>
-            <th scope="column" id="March">
-              March
-            </th>
-            <th scope="column" id="April">
-              April
-            </th>
-          </tr>
-          <tr>
-            <th scope="row">Week 1</th>
-            <td headers="January" onClick={handleClick} className="bad"></td>
-            <td headers="February" onClick={handleClick} className="fair"></td>
-            <td headers="March" onClick={handleClick} className="good"></td>
-            <td headers="April" onClick={handleClick} className="good"></td>
-          </tr>
-          <tr>
-            <th scope="row">Week 2</th>
-            <td headers="January" onClick={handleClick} className="fair"></td>
-            <td headers="February" onClick={handleClick} className="fair"></td>
-            <td headers="March" onClick={handleClick} className="good"></td>
-            <td headers="April" onClick={handleClick} className="bad"></td>
-          </tr>
-          <tr>
-            <th scope="row">Week 3</th>
-            <td headers="January" onClick={handleClick} className="bad"></td>
-            <td headers="February" onClick={handleClick} className="good"></td>
-            <td headers="March" onClick={handleClick} className="good"></td>
-            <td headers="April" onClick={handleClick} className="fair"></td>
-          </tr>
-          <tr>
-            <th scope="row">Week 4</th>
-            <td headers="January" onClick={handleClick} className="bad"></td>
-            <td headers="February" onClick={handleClick} className="good"></td>
-            <td headers="March" onClick={handleClick} className="fair"></td>
-            <td headers="April" onClick={handleClick} className="good"></td>
-          </tr>
+          <tbody>
+            <tr>
+              <th className="table-corner"></th>
+              <th scope="column" id="January">
+                January
+              </th>
+              <th scope="column" id="February">
+                February
+              </th>
+              <th scope="column" id="March">
+                March
+              </th>
+              <th scope="column" id="April">
+                April
+              </th>
+            </tr>
+            <tr>
+              <th scope="row">Week 1</th>
+              <td headers="January" onClick={handleClick} className="bad"></td>
+              <td headers="February" onClick={handleClick} className="fair"></td>
+              <td headers="March" onClick={handleClick} className="good"></td>
+              <td headers="April" onClick={handleClick} className="good"></td>
+            </tr>
+            <tr>
+              <th scope="row">Week 2</th>
+              <td headers="January" onClick={handleClick} className="fair"></td>
+              <td headers="February" onClick={handleClick} className="fair"></td>
+              <td headers="March" onClick={handleClick} className="good"></td>
+              <td headers="April" onClick={handleClick} className="bad"></td>
+            </tr>
+            <tr>
+              <th scope="row">Week 3</th>
+              <td headers="January" onClick={handleClick} className="bad"></td>
+              <td headers="February" onClick={handleClick} className="good"></td>
+              <td headers="March" onClick={handleClick} className="good"></td>
+              <td headers="April" onClick={handleClick} className="fair"></td>
+            </tr>
+            <tr>
+              <th scope="row">Week 4</th>
+              <td headers="January" onClick={handleClick} className="bad"></td>
+              <td headers="February" onClick={handleClick} className="good"></td>
+              <td headers="March" onClick={handleClick} className="fair"></td>
+              <td headers="April" onClick={handleClick} className="good"></td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <DataBreakdown summaryData={summaryData} theme={props.theme} />
