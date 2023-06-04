b785ca876fb3da62994d02f695e32f5848705295
mr-jackpot <adam.heeps@live.co.uk>
2023-06-04 12:42:28 +0100

    Returning tags based on score

--- web/src/components/data-breakdown/dataBreakdown.js
+++ web/src/components/data-breakdown/dataBreakdown.js
@@ -6,6 +6,43 @@ const transformScore = (score) => {
   return score.charAt(0).toUpperCase() + score.slice(1).toLowerCase();
 };
 
+const returnTagList = (score) => {
+  if (score === "good") {
+    return (
+        <div className={"tag-container"}>
+          <TagIcon feeling="friends" noclick/>
+          <TagIcon feeling="relationships" noclick/>
+          <TagIcon feeling="learning" noclick/>
+          <TagIcon feeling="motivation" noclick/>
+          <TagIcon feeling="bullish" noclick/>
+          <TagIcon feeling="future" noclick/>
+          <TagIcon feeling="work" noclick />
+        </div>
+    )
+  } else if (score === "fair") {
+    return (
+        <div className={"tag-container"}>
+          <TagIcon feeling="stressed" noclick/>
+          <TagIcon feeling="motviation" noclick/>
+          <TagIcon feeling="friends" noclick/>
+          <TagIcon feeling="acheivements" noclick/>
+          <TagIcon feeling="learning" noclick/>
+        </div>
+    )
+  } else {
+    return (
+        <div className={"tag-container"}>
+          <TagIcon feeling="stressed" noclick/>
+          <TagIcon feeling="negative" noclick/>
+          <TagIcon feeling="bearish" noclick/>
+          <TagIcon feeling="work" noclick/>
+          <TagIcon feeling="relationships" noclick/>
+          <TagIcon feeling="apathy" noclick/>
+        </div>
+    )
+  }
+}
+
 const DataBreakdown = (props) => {
   return (
     <div className={`data-breakdown ${props.theme}`}>
@@ -18,7 +55,7 @@ const DataBreakdown = (props) => {
           <div className="score-message">{transformScore(props.summaryData.score)}</div>
           <div className="title">Themes for week</div>
           <div className="tag-container">
-            <TagIcon feeling="shit" noclick/>
+            {returnTagList(props.summaryData.score.toLowerCase())}
           </div>
         </div>
       ) : (
